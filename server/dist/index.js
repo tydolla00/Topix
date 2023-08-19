3eec17175e74c8a36a83f9802c576ee293b546ba
tydolla00 <90355178+tydolla00@users.noreply.github.com>
2023-08-19 14:35:19 -0400

    wired up API's

--- server/dist/index.js
+++ server/dist/index.js
@@ -40,19 +40,22 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 (0, mountRoutes_1.default)(app);
 // ! Uncomment code below to create db schema.
-// const createTables = [
-//   "CREATE TABLE IF NOT EXISTS SCHEMA(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, type VARCHAR(10) CHECK (type = 'TV' OR type='MOVIE' OR TYPE='GAME'), name varchar(100), path varchar(100))",
-//   "CREATE TABLE IF NOT EXISTS USERS(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, users_id INT, username VARCHAR(50), email VARCHAR(50), password VARCHAR(60), first_name VARCHAR(30), last_name VARCHAR(30), profile_picture varchar(100),CONSTRAINT fk_users FOREIGN KEY(users_id) REFERENCES schema(id))",
-//   "CREATE TABLE IF NOT EXISTS MOVIES(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, movies_id INT, name VARCHAR(100) NOT NULL, genre VARCHAR(100) NOT NULL, timeline DATE NOT NULL, CONSTRAINT fk_movies FOREIGN KEY(movies_id) REFERENCES schema(id))",
-//   "CREATE TABLE IF NOT EXISTS TV(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, tv_id INT, name VARCHAR(100) NOT NULL, genre VARCHAR(100) NOT NULL, channel VARCHAR(100) NOT NULL, timeline DATE NOT NULL, CONSTRAINT fk_tv FOREIGN KEY(tv_id) REFERENCES schema(id))",
-//   "CREATE TABLE IF NOT EXISTS GAMES(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, games_id INT, name VARCHAR(100) NOT NULL, brand VARCHAR(100), timeline DATE NOT NULL, CONSTRAINT fk_games FOREIGN KEY(games_id) REFERENCES schema(id))",
-//   "CREATE TABLE IF NOT EXISTS TOPICS(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name VARCHAR(100) NOT NULL, description VARCHAR(1000) NOT NULL, path varchar(100))",
-//   "CREATE TABLE IF NOT EXISTS QUIZZES(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name VARCHAR(100) NOT NULL, description VARCHAR(1000) NOT NULL, path varchar(100))",
-//   "CREATE TABLE IF NOT EXISTS CONTACT(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, first_name VARCHAR(30) NOT NULL, last_name VARCHAR(30) NOT NULL, email VARCHAR(50) NOT NULL, subject VARCHAR(30) NOT NULL, message VARCHAR(3000) NOT NULL, CONSTRAINT subject_type CHECK (type IN ('Bug Fixes', 'Suggestions','Talk to me about your experience', 'Other'))
-//   "ALTER TABLE USERS ADD UNIQUE (USERNAME)",
-//   "ALTER TABLE USERS ADD UNIQUE (email)",
-// ];
-// const db = Promise.all([createTables.map((query) => queryNoCall(query))]);
+/*
+const createTables = [
+  "CREATE TABLE IF NOT EXISTS SCHEMA(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, type VARCHAR(10) CHECK (type = 'TV' OR type='MOVIE' OR TYPE='GAME'), name varchar(100), path varchar(100))",
+  "CREATE TABLE IF NOT EXISTS USERS(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, users_id INT, username VARCHAR(50), email VARCHAR(50), password VARCHAR(60), first_name VARCHAR(30), last_name VARCHAR(30), profile_picture varchar(100),CONSTRAINT fk_users FOREIGN KEY(users_id) REFERENCES schema(id))",
+  "CREATE TABLE IF NOT EXISTS MOVIES(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, movies_id INT, name VARCHAR(100) NOT NULL, genre VARCHAR(100) NOT NULL, timeline DATE NOT NULL, CONSTRAINT fk_movies FOREIGN KEY(movies_id) REFERENCES schema(id))",
+  "CREATE TABLE IF NOT EXISTS TV(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, tv_id INT, name VARCHAR(100) NOT NULL, genre VARCHAR(100) NOT NULL, channel VARCHAR(100) NOT NULL, timeline DATE NOT NULL, CONSTRAINT fk_tv FOREIGN KEY(tv_id) REFERENCES schema(id))",
+  "CREATE TABLE IF NOT EXISTS GAMES(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, games_id INT, name VARCHAR(100) NOT NULL, brand VARCHAR(100), timeline DATE NOT NULL, CONSTRAINT fk_games FOREIGN KEY(games_id) REFERENCES schema(id))",
+  "CREATE TABLE IF NOT EXISTS TOPICS(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name VARCHAR(100) NOT NULL, description VARCHAR(1000) NOT NULL, path varchar(100))",
+  "CREATE TABLE IF NOT EXISTS QUIZZES(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name VARCHAR(100) NOT NULL, description VARCHAR(1000) NOT NULL, path varchar(100))",
+  "CREATE TABLE IF NOT EXISTS CONTACT (id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,first_name VARCHAR(30) NOT NULL,last_name VARCHAR(30) NOT NULL,email VARCHAR(50) NOT NULL,subject VARCHAR(30) NOT NULL CHECK (subject IN ('Bug Fixes','Talk to me about your experience','Suggestions','Other')),message VARCHAR(3000) NOT NULL);",
+  "ALTER TABLE USERS ADD UNIQUE (USERNAME)",
+  "ALTER TABLE USERS ADD UNIQUE (email)",
+];
+
+const db = Promise.all([createTables.map((query) => queryNoCall(query))]);
+*/
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
