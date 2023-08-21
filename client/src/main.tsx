0e3151a4b65ee2635de47b2779f3e46a84ea44ea
tydolla00 <90355178+tydolla00@users.noreply.github.com>
2023-08-20 21:34:08 -0400

    Resolved errors

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { AuthProvider } from "./hooks/useAuth.tsx";
+import { AuthProvider, UserAuthData } from "./hooks/useAuth.tsx";
 import { getFromLocalStorage } from "./hooks/useLocalStorage.ts";
 
-const authData = getFromLocalStorage("user");
+const authData = getFromLocalStorage<UserAuthData>("user");
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider storedAuthData={authData}>
